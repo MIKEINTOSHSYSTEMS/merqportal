b1946650ea35770e593010327718931a4e7edcc8
MIKEINTOSHSYSTEMS <mikeintoshsys@gmail.com>
2025-08-21 21:52:28 +0300

    updated Production BASE_URL configuration for timesheet

--- apps/timesheet/config/config.php
+++ apps/timesheet/config/config.php
@@ -15,8 +15,8 @@ require_once __DIR__ . '/../../../includes/session-config.php';
 define('APP_NAME', 'MERQ Timesheet');
 define('APP_VERSION', '1.0.0');
 //define('BASE_URL', 'https://timesheet.merqconsultancy.org/timesheet');
-//define('BASE_URL', 'https://app.merqconsultancy.org/apps/timesheet');
-define('BASE_URL', 'http://merqapp/apps/timesheet');
+define('BASE_URL', 'https://app.merqconsultancy.org/apps/timesheet'); //Production BASE_URL
+//define('BASE_URL', 'http://merqapp/apps/timesheet');
 
 // Default calendar preference (will be handled with session-config.php)
 $_SESSION['ethiopian_calendar'] = $_SESSION['ethiopian_calendar'] ?? false;
