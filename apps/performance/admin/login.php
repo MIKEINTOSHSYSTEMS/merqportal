b3ffa74cab1a273dac2889cf3607ed329383c946
MIKEINTOSHSYSTEMS <mikeintoshsys@gmail.com>
2025-09-11 23:07:08 +0300

    updated fontawesome link for login page on performance apps module

--- apps/performance/admin/login.php
+++ apps/performance/admin/login.php
@@ -24,7 +24,7 @@ if (isset($_SESSION['error'])) {
     <title>Login - MERQ Performance Evaluation System</title>
     <link rel="icon" type="image/x-icon" href="/assets/images/merq-logo.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome@6.0.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <style>
         :root {
             --primary-color: #003366;
